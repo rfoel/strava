7e407cb1f2173920c5609f74e2827890cfa72e45
Rafael Franco <rafaelfr@outlook.com>
2020-12-01 18:12:50 -0300

    feat: add route methods

---
+++ src/enums/routeSubType.ts
@@ -0,0 +1,7 @@
+export enum RouteSubType {
+  Road = 1,
+  MountainBike = 2,
+  Cross = 3,
+  Trail = 4,
+  Mixed = 5,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Request } from './request'
-import { Activities, Athletes, Clubs, Gears } from './resources'
+import { Activities, Athletes, Clubs, Gears, Routes } from './resources'
 import { RefreshTokenRequest } from './types'
 
 export class Strava {
@@ -8,6 +8,7 @@ export class Strava {
   athletes: Athletes
   clubs: Clubs
   gears: Gears
+  routes: Routes
 
   constructor(config: RefreshTokenRequest) {
     this.request = new Request(config)
@@ -15,5 +16,6 @@ export class Strava {
     this.athletes = new Athletes(this.request)
     this.clubs = new Clubs(this.request)
     this.gears = new Gears(this.request)
+    this.routes = new Routes(this.request)
   }
 }
