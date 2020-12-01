79a117e2fbd2d3becd8704f4d2a17b9c2d61773a
Rafael Franco <rafaelfr@outlook.com>
2020-12-01 17:01:59 -0300

    feat: add athlete methods

---
+++ src/resources/athletes.ts
@@ -0,0 +1,44 @@
+import { ActivityStats, DetailedAthlete, Zones } from '../models'
+import { Request } from '../request'
+
+type getStatsRequest = {
+  id: number
+}
+
+type UpdateLoggedInAthleteRequest = {
+  weight?: number
+}
+
+export class Athletes {
+  private readonly request: Request
+
+  constructor(request) {
+    this.request = request
+  }
+
+  async getLoggedInAthlete(): Promise<DetailedAthlete> {
+    return await this.request.makeApiRequest<DetailedAthlete>('get', '/athlete')
+  }
+
+  async getLoggedInAthleteZones(): Promise<Zones> {
+    return await this.request.makeApiRequest<Zones>('get', '/athlete/zones')
+  }
+
+  async getStats(params: getStatsRequest): Promise<ActivityStats> {
+    const { id } = params
+    return await this.request.makeApiRequest<ActivityStats>(
+      'get',
+      `/athletes/${id}/stats`,
+    )
+  }
+
+  async updateLoggedInAthlete(
+    params: UpdateLoggedInAthleteRequest,
+  ): Promise<DetailedAthlete> {
+    return await this.request.makeApiRequest<DetailedAthlete>(
+      'put',
+      `/athlete`,
+      { body: params },
+    )
+  }
+}
