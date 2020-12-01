6aed8e1562e48e2cbeebc1c3eabb24f5e127cf4b
Rafael Franco <rafaelfr@outlook.com>
2020-12-01 20:31:12 -0300

    feat: add stream methods

---
+++ src/resources/streams.ts
@@ -0,0 +1,71 @@
+import { StreamKeys } from '../enums'
+import { StreamSet } from '../models'
+import { Request } from '../request'
+
+type GetActivityStreamsRequest = {
+  id: number
+  keys: StreamKeys[]
+}
+
+type GetRouteStreamsRequest = {
+  id: number
+}
+
+type GetSegmentEffortStreamsRequest = {
+  id: number
+  keys: StreamKeys[]
+}
+
+type GetSegmentStreamsRequest = {
+  id: number
+  keys: StreamKeys[]
+}
+
+export class Streams {
+  private readonly request: Request
+
+  constructor(request) {
+    this.request = request
+  }
+
+  async getActivityStreams(
+    params: GetActivityStreamsRequest,
+  ): Promise<StreamSet> {
+    const { id, ...query } = params
+    return await this.request.makeApiRequest<StreamSet>(
+      'get',
+      `/activities/${id}/streams`,
+      { query: { ...query, key_by_type: true } },
+    )
+  }
+
+  async getRouteStreams(params: GetRouteStreamsRequest): Promise<StreamSet> {
+    const { id } = params
+    return await this.request.makeApiRequest<StreamSet>(
+      'get',
+      `/routes/${id}/streams`,
+    )
+  }
+
+  async getSegmentEffortStreams(
+    params: GetSegmentEffortStreamsRequest,
+  ): Promise<StreamSet> {
+    const { id, ...query } = params
+    return await this.request.makeApiRequest<StreamSet>(
+      'get',
+      `/segment_efforts/${id}/streams`,
+      { query: { ...query, key_by_type: true } },
+    )
+  }
+
+  async getSegmentStreams(
+    params: GetSegmentStreamsRequest,
+  ): Promise<StreamSet> {
+    const { id, ...query } = params
+    return await this.request.makeApiRequest<StreamSet>(
+      'get',
+      `/segments/${id}/streams`,
+      { query: { ...query, key_by_type: true } },
+    )
+  }
+}
